c081082647ae82eb6af861d7d106c9151e9efa79
Benoit Blanchon <github@benoitblanchon.fr>
2014-06-20 15:36:49 +0200

    Fixed plots. Added product page.

--- prices/urls.py
+++ prices/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from prices import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.product, name='product')
 )
--- prices/views.py
+++ prices/views.py
@@ -1,7 +1,55 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
 from prices.models import Product
 
-class IndexView(generic.ListView):
-	template_name = 'prices/index.html'
-	model = Product
+def index(request):
+	products = []
+	for p in Product.objects.all():
+		references = []
+		for r in p.reference_set.all():
+			price = r.price_set.order_by("-end_time").first().price
+			references.append({
+				'name': r.name,
+				'shop': r.shop.name,
+				'price': price
+				})
+		products.append({
+			'name': p.name,
+			'references': references,
+			'id': p.id
+			})
+	context = {'products': products}
+	return render(request, 'prices/index.html', context)
+
+def product(request, pk):
+	p = get_object_or_404(Product, pk=pk)
+
+	references = []
+	for r in p.reference_set.all():
+		history = []
+		for x in r.price_set.order_by("start_time"):
+			history.append(
+				{
+					'time': x.start_time,
+					'price': x.price
+				})
+			history.append(
+				{
+					'time': x.end_time,
+					'price': x.price
+				})
+		references.append(
+			{
+				'name': r.name,
+				'shop': r.shop.name,
+				'history': history
+			})
+	product = {
+	 'name': p.name,
+	 'references': references
+	}
+
+	context = {'product': product}
+
+	return render(request, 'prices/product.html', context)
+
+
